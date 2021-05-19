d67d8d058afad20e32c3ed85318c016a65eb43e5
Borek Požár <pozar.borek@gmail.com>
2021-05-19 13:01:09 +0200

    Split parse_map_entities into two functions. Added function drop_subsets, which looks into all concurrent matched entities and drops one, if it matches a subset of indices when compared to the second one.

--- entities.py
+++ entities.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from copy import deepcopy
 
 # example response to work with
 response =  {
@@ -86,9 +87,9 @@ response =  {
 
 
 
-def parse_map_entities(response: dict):
+def parse_merge_entities(response: dict):
     """
-    Function to parse and map entities to input string.
+    Parses entities from Watson response into more usable dict.
     If one entity is found in consecutive intervals, the intervals are merged 
     and entity is returned only once in the result.
 
@@ -99,11 +100,7 @@ def parse_map_entities(response: dict):
     Returns:
     entities (dict): 
         Dictionary entities[entity_type][value] = {Given entity information in dict}
-
-    char_ents_mapping (dict): 
-        Dictionary char_ents_mapping[entity_type][char_index] = [List of entity values mapped to this index]
     """
-    char_ents_mapping = defaultdict(dict)
     entities = defaultdict(dict)
     for entity in response["entities"]:
         # retrieve the values
@@ -129,15 +126,107 @@ def parse_map_entities(response: dict):
         else:
             entities[entity_type][value] = {'value': value, 'location': (loc_start, loc_end), 'confidence': confidence}
 
-        # add to the char-entities mapping
-        for i in range(loc_start, loc_end):
-            if not i in char_ents_mapping[entity_type]:
-                char_ents_mapping[entity_type][i] = []
-            char_ents_mapping[entity_type][i].append(value)
-
-    return entities, char_ents_mapping
+    return entities
 
 # try on the example response
-entities, char_ents_mapping = parse_map_entities(response)
-print(char_ents_mapping)
+entities = parse_merge_entities(response)
 print(entities)
+
+
+
+def get_entity_char_mapping(entities: dict):
+    """
+    Maps entities to indices in input string.
+
+    Parameters:
+    entities (dict): The dict as return from parse_merge_entities,
+    i.e. entities[entity_type][value] = {Given entity information in dict}
+
+
+    Returns:
+    char_ents_mapping (dict): 
+        Dictionary char_ents_mapping[entity_type][char_index] = [List of entity values mapped to this index]
+    """
+    char_ents_mapping = defaultdict(dict)
+    # go through all entities
+    for entity_type in entities:
+        for value in entities[entity_type]:
+            # get the location
+            loc_start, loc_end = entities[entity_type][value]["location"]
+            # add to the char-entities mapping
+            for i in range(loc_start, loc_end):
+                if not i in char_ents_mapping[entity_type]:
+                    char_ents_mapping[entity_type][i] = []
+                char_ents_mapping[entity_type][i].append(value)
+
+    return char_ents_mapping
+
+# try on the example response
+char_ents_mapping = get_entity_char_mapping(entities)
+print(char_ents_mapping)
+
+
+
+def drop_subsets(entities, mapping):
+    """
+    Drops entities, which map to a subset of indices of another entity of the same type.
+    When in the input string is "name surname", the first entity matches only the "name" and 
+    the second entity matches the whole "name surname", we can drop the first one.
+
+    Parameters:
+    entities (dict): The dict of entities as return from parse_merge_entities,
+    i.e. entities[entity_type][value] = {Given entity information in dict}
+
+    mapping (dict): The dict of mapping as return from get_entity_char_mapping, 
+    i. e. mapping[entity_type][char_index] = [List of entity values mapped to this index]
+
+
+    Returns:
+    entities (dict): Dict in the same form as input with subset entities dropped.
+    """
+    entities_copy = deepcopy(entities)
+    for entity, entity_dict in entities_copy.items():
+        entity_start, entity_end = entity_dict["location"][0], entity_dict["location"][1]
+        possible_conc = set()
+        # check all indices to which this entity is mapped
+        for i in range(entity_start, entity_end):
+            # add entities mapped to this index as possible concurrents
+            for possible in mapping[i]:
+                if (possible in entities) and (not possible in possible_conc):
+                    possible_conc.add(possible)
+        # check all possible concurrents, delete if one is subset
+        for possible in possible_conc:
+            if possible in entities:
+                possible_start, possible_end = entities[possible]["location"][0], entities[possible]["location"][1]
+                # the possible is subset of this
+                if (possible_start >= entity_start and possible_end < entity_end) or (possible_start > entity_start and possible_end <= entity_end):
+                    del entities[possible]
+                    continue
+                # this is subset of the possible
+                elif (entity_start >= possible_start and entity_end < possible_end) or (entity_start > possible_start and entity_end <= possible_end):
+                    del entities[entity]
+
+    return entities
+        
+# try on the example response
+entities = drop_subsets(entities["name"], char_ents_mapping["name"])
+print(entities)
+
+
+
+# def select_biggest_matched(entities, mapping):
+#     to_resolve = set()
+#     for char_index in mapping:
+#         matched_ents = mapping[char_index]
+#         if len(matched_ents) == 1:
+#             # only one entity corresponding to index -> add if not there yet
+#             if not (value := matched_ents[0]) in to_resolve:
+#                 to_resolve.add(value)
+#         else:
+#             # more entities corresponding to index -> find biggest
+#             longest_match = None
+#             for value in matched_ents:
+#                 if value in entities:
+#                     if not longest_match:
+#                         longest_match = value
+#                     else:
