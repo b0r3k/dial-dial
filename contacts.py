c593eb83f8ab7e06bc613346f17a6419138024d2
Borek Požár <pozar.borek@gmail.com>
2021-05-24 12:20:57 +0200

    Example contact list as saved in WA entities.

---
+++ contacts.py
@@ -0,0 +1,84 @@
+
+
+# example contacts to work with, format es returned from WA workspace
+def return_test_contacts():
+    contacts = [
+        {
+          "type": "synonyms",
+          "value": "Řehoř Peříšek",
+          "synonyms": [
+            "Řehoř",
+            "Peříšek",
+            "Řepa"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Petr Svoboda",
+          "synonyms": [
+            "Peťa",
+            "Petr",
+            "Svoboda",
+            "Petru",
+            "Petrovi",
+            "Svobodovi"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Marie Dvořáková",
+          "synonyms": [
+            "Máňa",
+            "Marie",
+            "Dvořáková"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Jiří Novotný",
+          "synonyms": [
+            "Jirka",
+            "Jiří",
+            "Novotný"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Petr Nosek",
+          "synonyms": [
+            "Petr",
+            "Nosek",
+            "Petru",
+            "Petrovi",
+            "Noskovi"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Jan Novák",
+          "synonyms": [
+            "Honza",
+            "Jenda",
+            "Jan",
+            "Novák"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Jana Černá",
+          "synonyms": [
+            "Jana",
+            "Černá"
+          ]
+        },
+        {
+          "type": "synonyms",
+          "value": "Karolína Machová",
+          "synonyms": [
+            "Kája",
+            "Karolína",
+            "Machová"
+          ]
+        }
+      ]
+    return contacts
