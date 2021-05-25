39950d35dd88d1edb2e680c619478aa9eb10fdbf
Borek Požár <pozar.borek@gmail.com>
2021-05-25 15:07:02 +0200

    Deleted old code.

--- contacts.py
+++ contacts.py
@@ -1,89 +1,6 @@
 from collections import defaultdict
 
-# example contacts to work with, format es returned from WA workspace
-def return_test_contacts():
-    contacts = [
-        {
-          "type": "synonyms",
-          "value": "Řehoř Peříšek",
-          "synonyms": [
-            "Řehoř",
-            "Peříšek",
-            "Řepa"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Petr Svoboda",
-          "synonyms": [
-            "Peťa",
-            "Petr",
-            "Svoboda",
-            "Petru",
-            "Petrovi",
-            "Svobodovi"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Marie Dvořáková",
-          "synonyms": [
-            "Máňa",
-            "Marie",
-            "Dvořáková"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Jiří Novotný",
-          "synonyms": [
-            "Jirka",
-            "Jiří",
-            "Novotný"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Petr Nosek",
-          "synonyms": [
-            "Petr",
-            "Nosek",
-            "Petru",
-            "Petrovi",
-            "Noskovi"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Jan Novák",
-          "synonyms": [
-            "Honza",
-            "Jenda",
-            "Jan",
-            "Novák"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Jana Černá",
-          "synonyms": [
-            "Jana",
-            "Černá"
-          ]
-        },
-        {
-          "type": "synonyms",
-          "value": "Karolína Machová",
-          "synonyms": [
-            "Kája",
-            "Karolína",
-            "Machová"
-          ]
-        }
-      ]
-    return contacts
 
-#contacts = return_test_contacts()
 
 def wa_contacts_to_dict_and_list(contacts):
     """
@@ -108,6 +25,3 @@ def wa_contacts_to_dict_and_list(contacts):
         [cont_id_dict[name].append(id) for name in contact_names]
         cont_list.append(value)
     return cont_id_dict, cont_list
-
-#cont_id_dict = contacts_to_id_dict(contacts)
-#print(cont_id_dict)
