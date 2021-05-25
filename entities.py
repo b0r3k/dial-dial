fbb62df4b6506f3860368e53209d3116ad9ec80b
Borek Požár <pozar.borek@gmail.com>
2021-05-25 14:22:16 +0200

    Debugging - drop_subsets needed different range and some ordering, also when duplicate is found, the higher confidence is used now. Deleted old code.

--- entities.py
+++ entities.py
@@ -2,93 +2,7 @@ from collections import defaultdict, OrderedDict
 from copy import deepcopy
 from typing import Tuple
 
-# example response to work with
-def return_test_response():
-    response =  {
-    "intents": [
-        {
-        "intent": "send_money",
-        "confidence": 0.8539069175720215
-        }
-    ],
-    "entities": [
-        {
-        "entity": "name",
-        "location": [
-            10,
-            15
-        ],
-        "value": "Petr Nosek",
-        "confidence": 0.83
-        },
-        {
-        "entity": "name",
-        "location": [
-            10,
-            15
-        ],
-        "value": "Petr Svoboda",
-        "confidence": 0.83
-        },
-        {
-        "entity": "name",
-        "location": [
-            16,
-            25
-        ],
-        "value": "Petr Svoboda",
-        "confidence": 0.7
-        }
-    ],
-    "input": {
-        "text": "Pošli 100 Petru Svobodovi."
-    },
-    "output": {
-        "generic": [
-        {
-            "response_type": "text",
-            "text": "Nerozumím. Zkuste přeformulovat váš dotaz."
-        }
-        ],
-        "text": [
-        "Nerozumím. Zkuste přeformulovat váš dotaz."
-        ],
-        "nodes_visited": [
-        "V ostatních případech"
-        ],
-        "log_messages": []
-    },
-    "context": {
-        "metadata": {
-        "user_id": "07ba4cb2-9e12-4edf-b2aa-e629a6b79ed4"
-        },
-        "conversation_id": "07ba4cb2-9e12-4edf-b2aa-e629a6b79ed4",
-        "system": {
-        "initialized": True,
-        "dialog_stack": [
-            {
-            "dialog_node": "root"
-            }
-        ],
-        "dialog_turn_counter": 1,
-        "dialog_request_counter": 1,
-        "_node_output_map": {
-            "V ostatních případech": {
-            "0": [
-                0
-            ]
-            }
-        },
-        "last_branch_node": "V ostatních případech",
-        "branch_exited": True,
-        "branch_exited_reason": "completed"
-        }
-    },
-    "user_id": "07ba4cb2-9e12-4edf-b2aa-e629a6b79ed4"
-    }
-    return response
 
-# response = return_test_response()
 
 def parse_merge_same_entities(response: dict) -> dict:
     """
@@ -114,15 +28,21 @@ def parse_merge_same_entities(response: dict) -> dict:
 
         # add to the entities
         if entity_type in entities and value in entities[entity_type]:
-            # this entity was already recognized somewhere, try to join location intervals
-            # TODO what TODO with confidence? average? multiply?
+            # this entity was already recognized somewhere
+            # try to join location intervals
             old_loc_start, old_loc_end = entities[entity_type][value]["location"]
             if abs(loc_start - old_loc_end) <= 1:
                 # new one starts at the end of the old one, change end
                 entities[entity_type][value]["location"] = old_loc_start, loc_end
+                # use the higher confidence
+                if confidence > entities[entity_type][value]["confidence"]:
+                    entities[entity_type][value]["confidence"]
             elif abs(loc_end - old_loc_start) <= 1:
                 # new one ends at the beginning of the old one, change start
                 entities[entity_type][value]["location"] = loc_start, old_loc_end
+                # use the higher confidence
+                if confidence > entities[entity_type][value]["confidence"]:
+                    entities[entity_type][value]["confidence"]
             else:
                 # TODO two unrelated occurences of one entity in one input - what TODO?
                 pass
@@ -130,91 +50,6 @@ def parse_merge_same_entities(response: dict) -> dict:
             entities[entity_type][value] = {'value': value, 'location': (loc_start, loc_end), 'confidence': confidence}
 
     return entities
-
-# try on the example response
-# entities = parse_merge_entities(response)
-# print(entities)
-
-
-
-def get_entity_char_mapping(entities: dict) -> dict:
-    """
-    Maps entities to indices in input string.
-
-    Parameters:
-    entities (dict): The dict as return from parse_merge_entities,
-        i.e. entities[entity_type][value] = {Given entity information in dict}
-
-
-    Returns:
-    char_ents_mapping (dict): 
-        Dictionary char_ents_mapping[entity_type][char_index] = [List of entity values mapped to this index]
-    """
-    char_ents_mapping = defaultdict(dict)
-    # go through all entities
-    for entity_type in entities:
-        for value in entities[entity_type]:
-            # get the location
-            loc_start, loc_end = entities[entity_type][value]["location"]
-            # add to the char-entities mapping
-            for i in range(loc_start, loc_end):
-                if not i in char_ents_mapping[entity_type]:
-                    char_ents_mapping[entity_type][i] = []
-                char_ents_mapping[entity_type][i].append(value)
-
-    return char_ents_mapping
-
-# try on the example response
-# char_ents_mapping = get_entity_char_mapping(entities)
-# print(char_ents_mapping)
-
-
-
-def drop_subsets_of_type(entities: dict, mapping: dict) -> dict:
-    """
-    Drops entities, which map to a subset of indices of another entity of the same type.
-    When in the input string is "name surname", the first entity matches only the "name" and 
-    the second entity matches the whole "name surname", we can drop the first one.
-
-    Parameters:
-    entities (dict): The dict of entities of given type,
-        i.e. entities[value] = {Given entity information in dict}
-
-    mapping (dict): The dict of mapping of entities of given type, 
-        i.e. mapping[char_index] = [List of entity values of the type mapped to this index]
-
-
-    Returns:
-    entities (dict): Dict in the same form as input with subset entities dropped.
-    """
-    # create a copy to be able to manipulate the other dict
-    entities_copy = deepcopy(entities)
-    for entity, entity_dict in entities.items():
-        entity_start, entity_end = entity_dict["location"]
-        possible_conc = set()
-        # check all indices to which this entity is mapped
-        for i in range(entity_start, entity_end):
-            # add entities mapped to this index as possible concurrents
-            for possible in mapping[i]:
-                if (possible in entities_copy) and (not possible in possible_conc):
-                    possible_conc.add(possible)
-        # check all possible concurrents, delete if one is subset
-        for possible in possible_conc:
-            if possible in entities_copy:
-                possible_start, possible_end = entities_copy[possible]["location"]
-                # the possible is subset of this
-                if (possible_start >= entity_start and possible_end < entity_end) or (possible_start > entity_start and possible_end <= entity_end):
-                    del entities_copy[possible]
-                    continue
-                # this is subset of the possible
-                elif (entity_start >= possible_start and entity_end < possible_end) or (entity_start > possible_start and entity_end <= possible_end):
-                    del entities_copy[entity]
-
-    return entities_copy
-        
-# try on the example response
-# entities_name = drop_subsets_of_type(entities["name"], char_ents_mapping["name"])
-# print(entities_name)
 
 
 
@@ -267,12 +102,15 @@ def drop_subsets(entities: dict, starts: dict, ends: dict) -> dict:
     current = OrderedDict()
     entities_copy = deepcopy(entities)
     # go through input indices
-    for i in range(0, max(ends, key=ends.get)):
+    for i in range(0, max(ends) + 1):
         # check what ends on this index
         if i in ends:
             ended = ends[i]
+            # sort ending entities descending based on location start so that we remove the smallest first
+            loc_starts = [entities[ending]["location"][0] for ending in ended]
+            loc_starts_endings = sorted(zip(loc_starts, ended), reverse=True)
             # compare location with everything currently started
-            for ent in ended:
+            for _, ent in loc_starts_endings:
                 del current[ent]
                 ent_start, ent_end = entities[ent]["location"]
                 for conc in current:
@@ -330,7 +168,12 @@ def merge_different_consecutive(entities: dict, starts: dict, ends: dict) -> dic
                 new_location = (first["location"][0], second["location"][1])
                 # compute the confidence as average
                 new_confidence = round((first["confidence"] + second["confidence"]) / 2, 2)
-                # save it
-                entities_copy[new_value] = {"value": new_value, "location": new_location, "confidence": new_confidence}
+                # if duplicate, choose higher confidence
+                if new_value in entities_copy:
+                    if new_confidence > entities_copy[new_value]["confidence"]:
+                        entities_copy[new_value]["confidence"] = new_confidence
+                # if not, save it
+                else:
+                    entities_copy[new_value] = {"value": new_value, "location": new_location, "confidence": new_confidence}
     
     return entities_copy
