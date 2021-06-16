dcbab33c713959d9b817b9143b16640fd6bf7004
Borek Požár <pozar.borek@gmail.com>
2021-06-16 13:02:02 +0000

    Looks like build works, let's code (finally).

---
+++ mobile_app/app/src/main/java/com/b0r3k/dial_dial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.b0r3k.dial_dial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun OnCreate(savedInstanceState: Bundle?) {
+        super.OnCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
