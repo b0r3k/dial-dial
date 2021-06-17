7aa2af3ec283cb1cf74d21b9b794e77ddb426852
Borek Požár <pozar.borek@gmail.com>
2021-06-17 21:44:13 +0200

    Started new project, put mic image.

---
+++ mobile_app/app/src/main/java/com/b0r3k/dial_dial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.b0r3k.dial_dial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
