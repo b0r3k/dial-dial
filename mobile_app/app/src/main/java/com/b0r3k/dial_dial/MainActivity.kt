537d32576c9e4a84a62a996b267ae4eeca52a101
Borek Požár <pozar.borek@gmail.com>
2021-06-18 14:02:35 +0200

    After mic is clicked, permissions are resolved.

--- mobile_app/app/src/main/java/com/b0r3k/dial_dial/MainActivity.kt
+++ mobile_app/app/src/main/java/com/b0r3k/dial_dial/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.b0r3k.dial_dial
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.b0r3k.dial_dial.databinding.ActivityMainBinding
+import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions
+import androidx.appcompat.app.AlertDialog
+
 
 class MainActivity : AppCompatActivity() {
+
+    val requestPermissionLauncher =
+        registerForActivityResult(RequestMultiplePermissions()
+        ) { results: Map<String, Boolean> ->
+            if (results.values.all { it }) {
+                Toast.makeText(applicationContext, "Permissions granted.", Toast.LENGTH_SHORT).show()
+                // Call pipeline
+            } else {
+                showUnavailableDialog()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mainActBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        mainActBinding.ivSpeak.setOnClickListener {
+            if (checkPermissions()) {
+                // Call pipeline
+            }
+        }
+        setContentView(mainActBinding.root)
+    }
+
+
+    private fun checkPermissions(): Boolean {
+        val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.READ_CONTACTS)
+        when {
+            permissions.all { ActivityCompat.checkSelfPermission(applicationContext, it) == PackageManager.PERMISSION_GRANTED } -> {
+                return true
+            }
+            permissions.any { shouldShowRequestPermissionRationale(it) } -> {
+                showRationaleDialog(permissions)
+            }
+            else -> {
+                requestPermissionLauncher.launch(permissions)
+            }
+        }
+        return false
+    }
+
+    private fun showRationaleDialog(permissions: Array<String>) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Abychom vás mohli slyšet, potřebujeme přístup k mikrofonu. Abychom mohli zavolat lidem, potřebujeme přístup ke kontaktům.")
+            setTitle("Potřebujeme povolení!")
+            setPositiveButton("OK") { _, _ ->
+                requestPermissionLauncher.launch(permissions)
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showUnavailableDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Bohužel, bez přístupu k mikrofonu a kontaktům nemůžeme fungovat. Pokud chcete, přejděte do nastavení a udělte povolení.")
+            setTitle("Potřebujeme povolení!")
+            setPositiveButton("OK") { _, _ -> }
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
