24e86cc474fc2d00e14349ffb013e76475e878c7
Borek Požár <pozar.borek@gmail.com>
2021-06-22 10:20:36 +0200

    Started work on WA init.

---
+++ watson_init/workspace.py
@@ -0,0 +1,30 @@
+from ibm_watson import AssistantV1
+import json
+
+# Credentials need to be in a separate `ibm-credentials.env` file
+# as described in https://github.com/watson-developer-cloud/python-sdk#credential-file
+assistant = AssistantV1(version='2021-06-22')
+assistant.set_service_url('https://api.eu-de.assistant.watson.cloud.ibm.com')
+
+# Data
+name = "dial_dial"
+description = ""
+language = "cs"
+
+# Create empty workspace only with data above
+response = assistant.create_workspace(
+    name=name,
+    description=description,
+    language=language,
+    intents=[],
+    entities=[],
+    counterexamples=[],
+    metadata={})\
+        .get_result()
+
+# Print response
+print(json.dumps(response, indent=2, ensure_ascii=False))
+
+# Get the workspace id for further use
+workspace_id = response['workspace_id']
+print('Workspace id {0}'.format(workspace_id))
