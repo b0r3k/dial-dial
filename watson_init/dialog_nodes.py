ac4920c3efac9947c4ec02881612c7dbda6a9abb
Borek Požár <pozar.borek@gmail.com>
2021-06-23 15:28:13 +0200

    Using kwargs, can't figure out setting context.

--- watson_init/dialog_nodes.py
+++ watson_init/dialog_nodes.py
@@ -13,32 +13,53 @@ workspace_id = "d8a7cb3f-9268-401d-9f5a-0355d58616fb"
 dialog_nodes = []
 
 # Node for accepting contacts, then nothing
-id = "Contacts"
-title = "Přijmout kontakty"
-conditions = "@contacts"
-context = { "contacts": "<? input.text ?>"}
+node = {}
+node["dialog_node"] = "Contacts"
+node["title"] = "Přijmout kontakty"
+node["conditions"] = "@contacts"
+node["type"] = "standard"
+# context = { "contacts": "<? input.text ?>"}
+# node["context"] = DialogNodeContext(context)
+node = DialogNode(**node)
+dialog_nodes.append(node)
 
 # Node that detects user wants to call someone, then calls forwarding to webhook
-id = "Dial"
-title = "Zavolat někomu"
-conditions = "#dial"
-node_type = "standard"
-previous_sibling = "Contacts"
-next_step = DialogNodeNextStep(behavior="jump_to", dialog_node="Forward_webhook", selector="body")
+node = {}
+node["dialog_node"] = "Dial"
+node["title"] = "Zavolat někomu"
+node["conditions"] = "#dial"
+node["type"] = "standard"
+node["previous_sibling"] = "Contacts"
+node["next_step"] = DialogNodeNextStep(behavior="jump_to", dialog_node="Prepare_webhook", selector="body")
+node = DialogNode(**node)
+dialog_nodes.append(node)
 
 # Node that requires @name.values and $names?.length > 0, if satisfied forwards to webhook, else asks to fill
-id = "Prepare_webhook"
-title = "Připravit údaje pro webhook"
-node_type = "frame"
-parent = "Dial"
-next_step = DialogNodeNextStep(behavior="jump_to", dialog_node="Choose_person", selector="condition")
-context = {"input": "<? input.text ?>", "entities": "<? entities.toJson() ?>"}
+node = {}
+node["dialog_node"] = "Prepare_webhook"
+node["title"] = "Připravit údaje pro webhook"
+node["type"] = "frame"
+node["parent"] = "Dial"
+node["next_step"] = DialogNodeNextStep(behavior="jump_to", dialog_node="Choose_person", selector="condition")
+# context = {"input": "<? input.text ?>", "entities": "<? entities.toJson() ?>"}
+# node["context"] = DialogNodeContext(context)
+node = DialogNode(**node)
+dialog_nodes.append(node)
 
 # Node that calls the webhook, then prompts to call a contact or asks for further disambiguation 
-id = "Choose_person"
-title = "Vybrat osobu"
-node_type = "standard"
-parent = "Prepare_webhook"
-actions = [DialogNodeAction(name="main_webhook", type="webhook", parameters={"contacts": "$contacts", "input": "$input", "entities": "$entities"}, result_variable="webhook_result_1")]
-next_step = DialogNodeNextStep(behavior="jump_to", dialog_node="Forward_webhook", selector="user_input")
-conditions = "input != null && entities != null"
+node = {}
+node["dialog_node"] = "Choose_person"
+node["title"] = "Vybrat osobu"
+node["type"] = "standard"
+node["parent"] = "Prepare_webhook"
+node["actions"] = [DialogNodeAction(name="main_webhook", type="webhook", parameters={"contacts": "$contacts", "input": "$input", "entities": "$entities"}, result_variable="webhook_result_1")]
+node["next_step"] = DialogNodeNextStep(behavior="jump_to", dialog_node="Prepare_webhook", selector="user_input")
+node["conditions"] = "input != null && entities != null"
+node = DialogNode(**node)
+dialog_nodes.append(node)
+
+# Update the workspace
+response = assistant.update_workspace(workspace_id=workspace_id, dialog_nodes=dialog_nodes)
+
+# Print response
+print(response)
